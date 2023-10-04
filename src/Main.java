493c61469f4bafb20e84361a7579ddd2f2ce6d2d
leorjh <leonardojribas@gmail.com>
2023-10-03 22:41:12 -0300

    fix:new result

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import Challenges.TwoSum.TwoSum;
 public class Main {
     public static void main(String[] args) {
         TwoSum solution = new TwoSum();
-        int[] numbs = {3,2,5,8};
-        int target = 7;
+        int[] numbs = {3,2,5,8,10};
+        int target = 18;
         int[] result = solution.twoSum(numbs, target);
-        System.out.println("Expect Result: [1,2]");
+        System.out.println("Expect Result: [3,4]");
         System.out.println("Result: [" + result[0] + "," + result[1] + "]");
     }
 }
